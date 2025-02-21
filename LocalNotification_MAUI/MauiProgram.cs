db40749da73e80e199eaefeca084cc2cf9f983ce
Machadoggg <131879710+Machadoggg@users.noreply.github.com>
2025-02-21 12:16:28 -0500

    Install plugin and granted permission

--- LocalNotification_MAUI/MauiProgram.cs
+++ LocalNotification_MAUI/MauiProgram.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.Extensions.Logging;
+using Plugin.LocalNotification;
 
 namespace LocalNotification_MAUI
 {
@@ -9,6 +10,7 @@ namespace LocalNotification_MAUI
             var builder = MauiApp.CreateBuilder();
             builder
                 .UseMauiApp<App>()
+                .UseLocalNotification()
                 .ConfigureFonts(fonts =>
                 {
                     fonts.AddFont("OpenSans-Regular.ttf", "OpenSansRegular");
