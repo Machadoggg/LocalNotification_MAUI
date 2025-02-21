fdd67d3bb5069bfe41d5f1ae19d78cadc6893163
Machadoggg <131879710+Machadoggg@users.noreply.github.com>
2025-02-21 12:12:34 -0500

    Add project files.

---
+++ LocalNotification_MAUI/AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace LocalNotification_MAUI
+{
+    public partial class AppShell : Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ LocalNotification_MAUI/Platforms/MacCatalyst/AppDelegate.cs
@@ -0,0 +1,10 @@
+﻿using Foundation;
+
+namespace LocalNotification_MAUI
+{
+    [Register("AppDelegate")]
+    public class AppDelegate : MauiUIApplicationDelegate
+    {
+        protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+    }
+}
